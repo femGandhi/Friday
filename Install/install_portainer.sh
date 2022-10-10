aa7019e3ecc7d3e598158ab9b95c1f9bd398bb7f
Fem <fem.gandhi@gmail.com>
2022-10-10 01:36:45 -0400

    Update install_portainer.sh

--- Install/install_portainer.sh
+++ Install/install_portainer.sh
@@ -17,6 +17,9 @@ function check_internet() {
 
 check_internet
 
-sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+#sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
+#sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
 
+
+sudo docker pull portainer/portainer-ce:linux-amd64-2.15.1-alpine || error "Failed to pull latest Portainer docker image!"
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-amd64-2.15.1-alpine || error "Failed to run Portainer docker image!"
