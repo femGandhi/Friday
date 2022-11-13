43ecdece04ecb2a7b12a9765f44f71d779f51f85
Fem <fem.gandhi@gmail.com>
2022-11-12 23:50:50 -0500

    Update install_docker.sh

--- Install/install_docker.sh
+++ Install/install_docker.sh
@@ -18,6 +18,7 @@ function check_internet() {
 check_internet
 
 curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
+sudo groupadd docker
 sudo usermod -aG docker $USER || sudo gpasswd -a $USER docker || error "Failed to add user to the Docker usergroup."
 echo "Remember to logoff/reboot for the changes to take effect."
 
