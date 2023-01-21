eb69b96c0404951191916926a3544664322f9152
Fem <fem.gandhi@gmail.com>
2023-01-21 13:20:31 -0500

    Update install_docker.sh

--- Install/install_docker.sh
+++ Install/install_docker.sh
@@ -15,9 +15,30 @@ function check_internet() {
   fi
 }
 
+#Return 1 if container is running
+function check_container(){
+  container_name = $1
+  docker run container_name
+  if [ "$( docker container inspect -f '{{.State.Status}}' $container_name )" == "running" ]; then 
+    return 1
+  else
+    echo "Remember to logoff/reboot for the changes to take effect."
+    return 0
+  fi
+}
+
+
 check_internet
 
+###   https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
 curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
-sudo usermod -aG docker $USER || sudo gpasswd -a $USER docker || error "Failed to add user to the Docker usergroup."
-echo "Remember to logoff/reboot for the changes to take effect."
 
+### https://docs.docker.com/engine/install/linux-postinstall/
+#Usermod creates a usergroup called docker
+sudo usermod -aG docker $USER || sudo gpasswd -a $USER docker || error "Failed to add user to the Docker usergroup."
+
+#Activate changes to groups
+newgrp docker
+
+#Verify container is running
+check_container hello-world
